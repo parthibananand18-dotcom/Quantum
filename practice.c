129720a5867317c4cf79b4628df4f096be985eac
parthibananand18-dotcom <parthibananand18@gmail.com>
2026-06-30 23:43:01 +0530

    basic diff program added

--- practice.c
+++ practice.c
@@ -1,1 +1,12 @@
-Practice c programming language 
+#include<stdio.h>
+int main()
+{
+    int a,b,diff;
+    printf("Enter the value of A: ");
+    scanf("%d",&a);
+    printf("Enter the value of B: ");           
+    scanf("%d",&b); 
+    diff=a-b;
+    printf("The difference between A and B is: %d\n",diff);
+    return 0;   
+}
