5f074fd5864712bf24e1a2182a905aebc6d06b04
parthibananand18-dotcom <parthibananand18@gmail.com>
2026-07-08 16:30:59 +0530

    mistakes rectified

--- calculator.c
+++ calculator.c
@@ -1,33 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
   int num1,num2;
-  ch op;
+  char op;
   printf("Enter the value of num1: ");
-  scanf("%d",&num1);
+  scanf("%d", &num1);
   printf("Enter the value of num2: ");
-  scanf("%d",&num2);
+  scanf("%d", &num2);
   printf("Enter operator: ");
-  scanf("%c\n",&op);
-  if(op=='+')
+  scanf("\n%c", &op);
+  if (op =='+')
   {
-   printf("Sum:%d",num1+num2);
+    printf("Sum:%d",num1+num2);
   }
-  else if(op=='-')
+  else if (op=='-')
   {
-    printf(Diff:%d,num1-num2);
-      }
-  else if(op=='*')
+    printf("Diff:%d",num1-num2);
+  }
+  else if (op=='*')
   {
     printf("Product:%d",num1*num2);
   }
-  else if(op=='/')
+  else if (op=='/')
   {
     printf("Quotient:%d",num1/num2);
   }
   else
   {
-    prinf("Invalid operator");
+    printf("Invalid operator");
   }
   return 0;
-  }
+}
